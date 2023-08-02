8717394018ac137f86ab72e6be0c7093b3f2237b
darwish <darwish1994@outlook.com>
2023-08-03 01:21:56 +0300

    init project

---
+++ app/src/main/java/com/bank/currenyapp/app/App.kt
@@ -0,0 +1,17 @@
+package com.bank.currenyapp.app
+
+import android.app.Application
+import com.bank.currenyapp.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
